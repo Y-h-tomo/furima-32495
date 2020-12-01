72cdaf03a16ce0baa7dd5b2945aaaf16847a9d61
原口　善智 <ruge.e.azul@gmail.com>
2020-12-01 15:35:48 +0900

    ダミーユーザーデータの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true, format: { with: PASSWORD_REGEX }
-  validates :first_name, format: { with: /\A[ぁ-ん][ァ-ン][一-龥]+\z/ }
-  validates :last_name, format: { with: /\A[ぁ-ん][ァ-ン][一-龥]+\z/ }
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, format: { with: PASSWORD_REGEX}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
   validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/ }
   validates :birth_date, presence: true
