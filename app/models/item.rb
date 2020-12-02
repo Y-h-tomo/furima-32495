d2406181afcc636b7ee052ed83d61a51d4e15f6c
原口　善智 <ruge.e.azul@gmail.com>
2020-12-02 18:25:26 +0900

    商品出品時の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
-  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than: 300, less_than: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :shipping_cost_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_date_id, numericality: { other_than: 1 }
-
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-  validates :attribute, presence: true, length: { minimum: 300, maximum: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 end
