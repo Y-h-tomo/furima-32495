3af677b4f2a63eaa1f35d7fcf8c8cd25d09b002f
原口　善智 <ruge.e.azul@gmail.com>
2020-12-02 12:45:44 +0900

    アソシエーションおよびバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, numericality: { other_than: 1 }
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :attribute, presence: true, length: { minimum: 300, maximum: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 end
