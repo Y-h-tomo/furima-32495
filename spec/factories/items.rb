2ad76851e112e0bec5ac92e4d5820322d23ac1cc
原口　善智 <ruge.e.azul@gmail.com>
2020-12-03 00:13:31 +0900

    テスト用商品ダミーデータの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name              {Faker::Lorem.word}
+    description     {Faker::Lorem.sentence}
+    price                {Faker::Number.number(digits: 7)}
+    association :user
+    association :category
+    association :condition
+    association :shipping_cost
+    association :prefecture
+    association :shipping_date
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/item-sample.png'), filename: 'item-sample.png')
+    end
   end
 end
