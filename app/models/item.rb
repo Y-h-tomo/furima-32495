e12198d7c66603c9aebc386e569a33295855a926
原口　善智 <ruge.e.azul@gmail.com>
2020-12-02 14:04:33 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
