086e70e7ed6492507d6ccb481a3f778784758a8b
原口　善智 <ruge.e.azul@gmail.com>
2020-12-04 20:38:35 +0900

    ダミーorderデータの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  require 'faker'
+  Faker::Config.locale = :ja
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :purchase
   belongs_to_active_hash :prefecture
